ff3a3037efbb61f58f6a1bb41bc66273d0e5d0ba
Jeremy Krovitz <jkrovitz@gmail.com>
2019-05-06 17:13:59 -0500

    Added files used for streaming tweets in python.

---
+++ PYTHON_FILES_USED_TO_STREAM_DATA/streamHauntingOfSharonTate.py
@@ -0,0 +1,23 @@
+from tweepy.streaming import StreamListener
+from tweepy import OAuthHandler
+from tweepy import Stream
+
+consumer_key = '----FILL IN----'
+consumer_secret = '----FILL IN----'
+access_token = '----FILL IN----'
+access_token_secret = '----FILL IN----'
+
+class listener(StreamListener):
+
+    def on_data(self, data):
+        print(data)
+        return(True)
+
+    def on_error(self, status):
+        print (status)
+
+auth = OAuthHandler(consumer_key, consumer_secret)
+auth.set_access_token(access_token, access_token_secret)
+
+twitterStream = Stream(auth, listener())
+twitterStream.filter(track=["The Haunting of Sharon Tate", "HauntingOfTate", "The Haunting Of Sharon Tate", "THE HAUNTING OF SHARON TATE", "TheHauntingOfSharonTate", "TheHauntingofSharonTate"], languages=["en"])
---
+++ PYTHON_FILES_USED_TO_STREAM_DATA/streamTheCurseOfLaLlorona.py
@@ -0,0 +1,26 @@
+import tweepy
+from tweepy.streaming import StreamListener
+from tweepy import OAuthHandler
+from tweepy import Stream
+
+
+consumer_key = '----FILL IN----'
+consumer_secret = '----FILL IN----'
+access_token = '----FILL IN----'
+access_token_secret = '----FILL IN----'
+
+
+class listener(StreamListener):
+
+    def on_data(self, data):
+        print(data)
+        return(True)
+
+    def on_error(self, status):
+        print (status)
+
+auth = OAuthHandler(consumer_key, consumer_secret)
+auth.set_access_token(access_token, access_token_secret)
+
+twitterStream = Stream(auth, listener())
+twitterStream.filter(track=["The Curse of La Llorona", "the Curse of La Llorona", 'La Llorona', '#LaLlorona' , 'lalloronamovie'], languages=["en"])
